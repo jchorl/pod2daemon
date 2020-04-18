6fc14dea2d08ffa7ed7543f634052560369c1783
Josh Chorlton <jchorlton@gmail.com>
2020-04-18 11:23:30 -0700

    push to latest-arm

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ echo "$2" | docker login -u "$1" --password-stdin
 
 docker images
 
-docker tag jchorl/pod2daemon-flexvol:latest-arm jchorl/pod2daemon-flexvol:latest
-docker push jchorl/pod2daemon-flexvol:latest
+docker push jchorl/pod2daemon-flexvol:latest-arm
